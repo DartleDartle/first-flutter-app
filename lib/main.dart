f7f20d2ea265554eadce7e481d8f72ad66996ec9
DartleDartle <davide.pintore98@gmail.com>
2024-08-28 16:29:19 +0200

    Enabled widget binding before firebase.initializeApp - Added Loading text.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
       title: 'Flutter Demo',
@@ -12,8 +16,31 @@ void main() {
     ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +49,52 @@ class HomePage extends StatelessWidget {
         title: const Text('Register', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: TextButton(onPressed: (){}, 
-        child: const Text('Register',)),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+                  options: DefaultFirebaseOptions.currentPlatform,
+                  ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+           
+            case ConnectionState.done:
+              return Column(
+          children: [
+            TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+              hintText: 'Enter your e-mail here',
+              ),
+            ),
+            TextField(
+              controller: _password,
+              obscureText: true,
+              enableSuggestions: false,
+              autocorrect: false,
+              decoration: InputDecoration(
+              hintText: 'Enter your password here',
+              ),
+            ),
+            TextButton(
+              onPressed: () async {
+                final email = _email.text;
+                final password = _password.text;
+                final userCredentials = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                print(userCredentials.user);
+              }, 
+              child: const Text('Register',)),
+          ],
+        );
+            default:
+            return const Text('Loading...');
+              }
+          
+        },
       ),
     );
   }
