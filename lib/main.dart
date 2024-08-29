b50e3fcb4286174a0e256d2971035a3726a9ecd4
DartleDartle <147179422+DartleDartle@users.noreply.github.com>
2024-08-29 13:21:59 +0200

    Added Homepage and started to ensure that every account has email verified.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firstapp/views/login_view.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firstapp/firebase_options.dart';
+import 'package:flutter/material.dart';
+
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,101 +13,41 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ));
 }
 
-  class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
+  class HomePage  extends StatelessWidget {
+  const HomePage ({super.key});
 
   @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-
- @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Register', style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.blue,
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-                  options: DefaultFirebaseOptions.currentPlatform,
-                  ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-           
-            case ConnectionState.done:
-              return Column(
-          children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-              hintText: 'Enter your e-mail here',
-              ),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: InputDecoration(
-              hintText: 'Enter your password here',
-              ),
-            ),
-            TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-                try{
-                final userCredentials = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                  email: email,
-                  password: password,
-                );
-                print(userCredentials.user);
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'weak-password') {
-                    print('The password provided is too weak.');
-                  } else if (e.code == 'email-already-in-use') {
-                    print('The account already exists for that email.');
-                  } else if (e.code == 'invalid-email') {
-                    print('The email provided is invalid.');
-                  }
-                }
-              }, 
-              child: const Text('Register',)),
-          ],
-        );
-            default:
-            return const Text('Loading...');
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Home', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.blue,
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+                    options: DefaultFirebaseOptions.currentPlatform,
+                    ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+            
+              case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('You are a verified user');
+              } else {
+                print('You need to verify your email first.');
               }
-          
-        },
-      ),
-    );
+                return Text('Done');
+              default:
+              return const Text('Loading...');
+                }
+            
+          },
+        ),
+      );
+    }
   }
-}
-
