68a2269fc6884b397bb5c1a1b02300ddc7573269
DartleDartle <davide.pintore98@gmail.com>
2024-09-02 12:54:32 +0200

    Added the good email verified check logic, now we got all for start up everything else.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firstapp/firebase_options.dart';
 import 'package:firstapp/views/login_view.dart';
 import 'package:firstapp/views/register_view.dart';
+import 'package:firstapp/views/verify_email_view.dart';
 import 'package:flutter/material.dart';
 
 
@@ -35,14 +36,17 @@ void main() {
           builder: (context, snapshot) {
              switch (snapshot.connectionState) {
               case ConnectionState.done:
-                /* final user = FirebaseAuth.instance.currentUser;
-                print(user);
-                if (user?.emailVerified ?? false) {
-                  return Text('Done');
+                final user = FirebaseAuth.instance.currentUser;
+                if (user != null) {
+                 if (user.emailVerified) {
+                   print('Email is verified');
+                 } else {
+                   return const VerifyEmailView();
+                 }
                 } else {
-                  return const VerifyEmailView();
-                }  */
-                return const LoginView();
+                  return const LoginView();
+                }  
+                return const Text('Done');
               default: 
                 return const CircularProgressIndicator();
             }
